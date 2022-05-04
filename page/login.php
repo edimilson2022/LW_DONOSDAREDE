4375d793ec2048780534a2822fdb7dc8026f048c
Lindenbergue <cicero.gomes23@aluno.ce.gov.br>
2022-05-04 11:30:11 -0300

    criado login

--- page/login.php
+++ page/login.php
@@ -1,12 +1,48 @@
+<!DOCTYPE html>
 <html>
 <head>
-<title> Cadastro de Usuário </title>
+<meta charset="UTF-8"/>
+<title>Document</title>
 </head>
 <body>
-<form method="POST" action="cadastro.php">
-<label>Login:</label><input type="text" name="login" id="login"><br>
-<label>Senha:</label><input type="password" name="senha" id="senha"><br>
-<input type="submit" value="Cadastrar" id="cadastrar" name="cadastrar">
-</form>
+<div class="container" >
+    <a class="links" id="paracadastro"></a>
+    <a class="links" id="paralogin"></a>
+    
+    <div class="content">      
+      <!--FORMULÁRIO DE LOGIN-->
+      <div id="login">
+        <form method="post" action=""> 
+          <h1>Login</h1> 
+          <p> 
+            <label for="email_login">Seu e-mail</label>
+            <input id="email_login" name="email_login" required="required" type="text" placeholder="contato@htmlecsspro.com"/>
+          </p>
+          
+          <p> 
+            <label for="senha_login">Sua senha</label>
+            <input id="senha_login" name="senha_login" required="required" type="password" placeholder="1234" /> 
+          </p>
+          
+          <p> 
+            <input type="checkbox" name="manterlogado" id="manterlogado" value="" /> 
+            <label for="manterlogado">Manter-me logado</label>
+          </p>
+          
+          <p> 
+            <input type="submit" value="Logar" /> 
+          </p>
+          
+          <p class="link">
+            Ainda não tem conta?
+            <a href="#paracadastro">Cadastre-se</a>
+          </p>
+        </form>
+      </div>
+     
+        </form>
+      </div>
+    </div>
+  </div> 
 </body>
 </html>
