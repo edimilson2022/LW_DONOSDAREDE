73400457d18d057a0c070dd225e75345e81d82b9
Jhenison <jose.bezerra66@aluno.ce.gov.br>
2022-05-04 11:51:02 -0300

    Consertando erro de LINDO

--- page/cadUsuario.php
+++ page/cadUsuario.php
@@ -1,18 +1,3 @@
-<<<<<<< HEAD
- <?php
-   include 'conexao.php';
-
-$login = $_POST['login'];
-$senha = MD5($_POST['senha']);
-$connect = mysql_connect('nome_do_servidor','nome_de_usuario','senha');
-$db = mysql_select_db('nome_do_banco_de_dados');
-$query_select = "SELECT login FROM usuarios WHERE login = '$login'";
-$select = mysql_query($query_select,$connect);
-$array = mysql_fetch_array($select);
-$logarray = $array['login'];
-
-   $sql ="INSERT INTO 'cadrastar'('nome','email','senha') VALUES ('nome','email','senha) 
-=======
 <!DOCTYPE html>
 <html>
 <head>
