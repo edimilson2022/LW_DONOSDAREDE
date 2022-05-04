cbb51fa018a8cc6a363249022063ffc1230ec4cf
Jhenison <jose.bezerra66@aluno.ce.gov.br>
2022-05-04 11:47:33 -0300

    commit idiota

--- page/cadUsuario.php
+++ page/cadUsuario.php
@@ -1,4 +1,5 @@
-<?php
+ <?php
+   include 'conexao.php';
 
 $login = $_POST['login'];
 $senha = MD5($_POST['senha']);
@@ -9,32 +10,4 @@ $select = mysql_query($query_select,$connect);
 $array = mysql_fetch_array($select);
 $logarray = $array['login'];
 
-  if($login == "" || $login == null){
-    echo"<script language='javascript' type='text/javascript'>
-    alert('O campo login deve ser preenchido');window.location.href='
-    cadastro.html';</script>";
-
-    }else{
-      if($logarray == $login){
-
-        echo"<script language='javascript' type='text/javascript'>
-        alert('Esse login já existe');window.location.href='
-        cadastro.html';</script>";
-        die();
-
-      }else{
-        $query = "INSERT INTO usuarios (login,senha) VALUES ('$login','$senha')";
-        $insert = mysql_query($query,$connect);
-
-        if($insert){
-          echo"<script language='javascript' type='text/javascript'>
-          alert('Usuário cadastrado com sucesso!');window.location.
-          href='login.html'</script>";
-        }else{
-          echo"<script language='javascript' type='text/javascript'>
-          alert('Não foi possível cadastrar esse usuário');window.location
-          .href='cadastro.html'</script>";
-        }
-      }
-    }
-?>
+   $sql ="INSERT INTO 'cadrastar'('nome','email','senha') VALUES ('nome','email','senha) 
