2c70983f8c50ccdd35e5289e79daf38649ff781a
Lindenbergue <cicero.gomes23@aluno.ce.gov.br>
2022-05-09 14:39:40 -0300

    comit de emergencia

--- index.php
+++ index.php
@@ -1,40 +1,21 @@
 
 <!DOCTYPE html>
 <html>
-<head>
-<meta charset="UTF-8"/>
-<title>Document</title>
-<link rel="stylesheet" href="css/style.css">
-</head>
-<body>
-
-<nav>
-<div class="text-box">
-<a class="btn btn-outline-danger" href="page/cadastro.php" role="button"  style="background-color:black; border-color:red; color:white">Cadastro</a>
-
-</nav>
-<nav>
-<a class="btn btn-primary" href="page/login.php" role="button" style="background-color:black; border-color:red; color:white">Login</a>
-</div>
-<style type="text/css">
-     
-     .text-box{
-   margin-left: 40vw;
-        margin-top: 30vh;
-   }
-   
-   .text-box1{
-   margin-left: 50vw;
-        margin-top: 50vh;
-   }
-     
-   h1{
-       
-       color: white;
-       font-family: "Open Sans Condesed", sans-serif;
-   }
-     </style>
-</nav>
-
-</body>
+     <head>
+          <meta charset="UTF-8"/>
+          <title>Página Inicial</title>
+          <link rel="stylesheet" href="css/style.css">
+     </head>
+     <body class="indice">
+          <div class="content">
+          <div id="menu">
+          <nav>
+               <div class="link">
+                    <a class="btn btn-outline-danger" href="./page/cadUsuario.php" role="button">Cadastro</a>
+                    <a class="btn btn-primary" href="./page/login.php" role="button">Login</a>
+               </div>
+          </nav>
+          </div>
+          </div>
+     </body>
 </html>
