3045b78646ccebe99d5e9f5ea5e22647e719f3be
Lindenbergue <cicero.gomes23@aluno.ce.gov.br>
2022-05-11 11:52:55 -0300

    entrar

--- controle/conexao.php
+++ controle/conexao.php
@@ -1,12 +1,3 @@
 <?php
      $servidor="localhost";
-     $usuario="root";
-     $senha="";
-     $dbname="projeto";
-
-     $conexao=mysqli_connct(servidor, $usuario, $senha, $dbname)
-     if(!$conexao){
-          die("houve um erro :".mysqli_connect_error());
-
-     }
- ?>
+     $usuario="root"
