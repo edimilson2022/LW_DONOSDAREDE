39d002c52c8212ad987b229e571c4673d1dcc3eb
Jhenison <jose.bezerra66@aluno.ce.gov.br>
2022-05-11 11:40:19 -0300

    criando conexão

---
+++ controle/conexao.php
@@ -0,0 +1,12 @@
+<?php
+     $servidor="localhost";
+     $usuario="root";
+     $senha="";
+     $dbname="projeto";
+
+     $conexao=mysqli_connct(servidor, $usuario, $senha, $dbname)
+     if(!$conexao){
+          die("houve um erro :".mysqli_connect_error());
+
+     }
+ ?>
