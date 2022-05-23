df4368c1417263785b983549ca7baf7ec677550e
Lindenbergue <cicero.gomes23@aluno.ce.gov.br>
2022-05-23 16:07:27 -0300

    a

--- page/areaCliente.php
+++ page/areaCliente.php
@@ -7,7 +7,84 @@
     <link rel="stylesheet" href="../css/style.css">
     <title>Área do Cliente</title>
 </head>
-<body>
-    
+<body> 
+  
+<h1>Área do Cliente</h1>
+
+<div class="imagem">
+ <img src="https://cdn.awsli.com.br/300x300/1861/1861887/produto/110891414/87f6989a5a.jpg" >
+ <div class="capa">
+   <h1>PREÇO</h1>
+   <p>129,90$</p>
+   <a class="btn btn-dark" href="paginadecompra.php" role="button" type="button" style="background-color:black; border-color:red; color:white">COMPRAR</a>
+  
+ </div>
+
+ <div class="imagem">
+ <img src="https://cdn.awsli.com.br/300x300/1973/1973315/produto/117528444/d6a7684a60.jpg" >
+ <div class="capa">
+   <h1>PREÇO</h1>
+   <p>159,90$</p>
+   <a class="btn btn-dark" href="paginadecompra.php" role="button" type="button" style="background-color:black; border-color:red; color:white">COMPRAR</a>
+
+ </div>
+ 
+<div class="imagem">
+ <img src="https://cdn.awsli.com.br/300x300/1973/1973315/produto/120728421/437986fcb8.jpg" >
+ <div class="capa">
+   <h1>PREÇO</h1>
+   <p>89,90</p>
+   <a class="btn btn-dark" href="paginadecompra.php" role="button" type="button" style="background-color:black; border-color:red; color:white">COMPRAR</a>
+   
+
+ </div>
+
+</div>
+
+ <div class="imagem">
+ <img src="https://imgs.casasbahia.com.br/1532855876/1xg.jpg?imwidth=292" >
+ <div class="capa">
+   <h1>PREÇO</h1>
+   <p>229,90$</p>
+   <a class="btn btn-dark" href="paginadecompra.php" role="button" type="button" style="background-color:black; border-color:red; color:white">COMPRAR</a>
+
+ </div>
+
+</div>
+<div class="imagem">
+ <img src="https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcR9LdvD3Nu2l5PlVdBTfStofpj_sqLisyuXvhIPnd-IrmqGSEcE-tkNKS8fXRw_JYsnveOYrsIt9wnXcyTqAdGMrG5mxc1BbI2-H8EWtfk&usqp=CAE" >
+ <div class="capa">
+   <h1>PREÇO</h1>
+   <p>229,90$</p>
+   <a class="btn btn-dark" href="paginadecompra.php" role="button" type="button" style="background-color:black; border-color:red; color:white">COMPRAR</a>
+
+ </div>
+
+</div>
+<style type="text/css">
+.imagem {
+  position: relative;
+}
+.capa {
+  display: none;
+}
+
+.imagem:hover .capa {
+  display: block;
+}
+
+.capa {
+  background: rgba(255, 255, 255, .1);
+
+  width: 10%;
+  height: 15%;
+  position: absolute;
+  top: 0;
+}
+
+</style> 
+</body>
+</html>
+
 </body>
 </html>
