cbf7c817f8a29d9c516cab1411e5b3982ac93dc8
lindenbergue10 <104520059+lindenbergue10@users.noreply.github.com>
2022-05-30 14:14:21 -0300

    Create listaUsuario.php

---
+++ page/listaUsuario.php
@@ -0,0 +1,18 @@
+<div class="container" style="border: 1px solid black">
+            <table class="content">
+                <thead class="link">
+                    <td class="">ID</td>
+                    <td>NOME</td>
+                    <td>EMAIL</td>
+                    <td>SENHA</td>
+                    <td><button onclick="">X</button></td>
+                </thead>
+                <tbody>
+                       <?php
+                       header('location:listaUsuario.php')
+
+                       ?>             
+                
+                </tbody>
+            </table>
+        </div>
